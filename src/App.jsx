ecc3a99d4c3a994217300170fb7b116d002b3e77
VinitCoding1628 <vinitcoding1628@gmail.com>
2026-09-24 11:37:58 +0530

    Initialization of the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='inter-font'>This is inter font</h2>
+      <h2 className='pt-serif-font'>This is PT Serif</h2>
+    </div>
+  )
+}
+
+export default App
