ecba2243c0cbae4eb9d17863bd0cdc9ea901050e
VinitCoding1628 <vinitcoding1628@gmail.com>
2026-09-24 15:41:50 +0530

    Completed navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import icon from '../../public/icons.svg'
+import { MdOutlineLightMode } from "react-icons/md";
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between items-center px-6 py-3 border-b-[0.1px] border-gray-300'>
+        <div className='flex justify-center items-center gap-4'>
+            <img src={icon} alt="" className='w-9'/>
+            <div>
+                <h2 className='text-2xl font-bold inter-font'>WordNest</h2>
+                <p className='text-gray-500 text-xs'>Discover • Understand • Speak</p>
+            </div>
+        </div>
+        <div className='bg-white p-2 rounded-3xl'>
+            <MdOutlineLightMode className='text-lg'/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
